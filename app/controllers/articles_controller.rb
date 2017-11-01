aa1bb16bf2af0b6ef2e2501ae3def3b4ca364fe6
Ayush Shukla <ashukla@groupon.com>
2017-11-01 21:13:31 +0530

    formatted all pages for easy visualization while development

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    puts "hello"
     @article = Article.find(params[:id])
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+
   #get 'users/index'
 
   get 'welcome/index'
