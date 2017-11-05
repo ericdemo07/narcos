d56c0a9861603f9c72b79fd39ccb6ed2f4f65afc
Ayush Shukla <ashukla@groupon.com>
2017-11-05 22:18:29 +0530

    removing unwanted validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,3 @@
 class Article < ApplicationRecord
   belongs_to :user
-
-  validates :first_name, presence: true,
-            length: { minimum: 5 }
-  validates :last_name, presence: true,
-            length: { minimum: 5 }
-  validates :city, presence: true
-
 end
